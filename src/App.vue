e4859450388e738ca27f8a85588649ca24fd0de9
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2020-01-07 20:07:52 +0900

    added site title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
-    <Graphql />
+    <v-container class="mt-5">
+      <div class="display-2 text-center">チェーン店メモったろう君</div>
+      <Graphql />
+    </v-container>
   </v-app>
 </template>
 
